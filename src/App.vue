2ee96c0c4000884b392167eb257d1fcbc6ca6556
Jesus Lozano <jalozano841@gmail.com>
2020-05-01 21:08:04 -0500

    Main Layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app clipped-left color="green darken-6">
+      <v-toolbar-title>VuePong</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app clipped color="grey darken-6"></v-navigation-drawer>
+    <v-content class="error noOverflow">
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 
